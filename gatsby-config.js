74c2f66e13fa9bba0f458b8bd12eea52544b65ed
alexant2 <Thomas.Alexander@umusic.com>
2021-05-25 00:22:11 -0700

    add gatsby bundle analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
+      options: {
+        devMode: false,
+        analyzerMode: "static"
+      }
+    }
   ],
 }
