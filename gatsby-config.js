3204fe251952ef7cd1c364a81b725ee9e35bc5f8
alexant2 <Thomas.Alexander@umusic.com>
2021-05-25 10:40:54 -0700

    use different demo app

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,52 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken:
+    process.env.CONTENTFUL_ACCESS_TOKEN ||
+    process.env.CONTENTFUL_DELIVERY_TOKEN,
+};
+
+// If you want to use the preview API please define
+// CONTENTFUL_HOST and CONTENTFUL_PREVIEW_ACCESS_TOKEN in your
+// environment config.
+//
+// CONTENTFUL_HOST should map to `preview.contentful.com`
+// CONTENTFUL_PREVIEW_ACCESS_TOKEN should map to your
+// Content Preview API token
+//
+// For more information around the Preview API check out the documentation at
+// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
+//
+// To change back to the normal CDA, remove the CONTENTFUL_HOST variable from your environment.
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST;
+  contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+}
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  );
+}
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby with Contentful`,
+    title: "Gatsby Contentful starter",
   },
+  pathPrefix: "/gatsby-contentful-starter",
   plugins: [
-    `gatsby-plugin-image`,
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `rocybtov1ozk`,
-        accessToken: `6f35edf0db39085e9b9c19bd92943e4519c77e72c852d961968665f1324bfc94`,
-      },
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig,
     },
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
-      options: {
-        devMode: false,
-        analyzerMode: "static"
-      }
-    }
   ],
-}
+};
